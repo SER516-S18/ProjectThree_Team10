0f9ad79057e3479ceaee2165c6c8b2e8b535f212
魏资童 <weizitongbupt@gmail.com>
2018-03-31 13:40:18 -0700

    Add static model for view using

--- client/controller/ClientController.java
+++ client/controller/ClientController.java
@@ -15,6 +15,12 @@ import client.service.*;
 public class ClientController {
     private Logger logger = Logger.getLogger(ClientController.class.getName());
     private Session session;
+    public static Parameters param;
+
+    static {
+        param = new Parameters(0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                false, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    }
 
     @OnOpen
     public void open(Session session) {
@@ -24,7 +30,19 @@ public class ClientController {
 
     @OnMessage
     public void onMessage(Parameters message) {
-        logger.info("Server send message - " + message.getTime());
+        param.setTime(message.getTime());
+        param.setNeutral(message.getNeutral());
+        param.setPushSkill(message.getPushSkill());
+        param.setOverallSkill(message.getOverallSkill());
+        param.setRaiseBrow(message.getRaiseBrow());
+        param.setSmile(message.getSmile());
+        param.setBlink(message.getBlink());
+        param.setInterest(message.getInterest());
+        param.setEngagement(message.getEngagement());
+        param.setStress(message.getStress());
+        param.setRelaxation(message.getRelaxation());
+        param.setExcitement(message.getExcitement());
+        param.setFocus(message.getFocus());
     }
 
     @OnClose
