4c20ea439ddcc254ff0469cbe2010d25228f191d
魏资童 <weizitongbupt@gmail.com>
2018-04-03 14:37:12 -0700

    Seperate Parametes class to Eye, LowerFace, MentalCmd, Performance and UpperFace classes.

--- client/model/Parameters.java
+++ client/model/Parameters.java
@@ -1,47 +1,27 @@
 package client.model;
 
+import client.mode.Eye;
+import client.mode.LowerFace;
+import client.mode.MentalCmd;
+import client.mode.Performance;
+import client.mode.UpperFace;
+
 public class Parameters {
+    private Eye eye;
+    private LowerFace lowerFace;
+    private UpperFace upperFace;
+    private MentalCmd mentalCmd;
+    private Performance performance;
     private double time;
-    private double neutral;
-    private double pushSkill;
-    private double overallSkill;
-    private double raiseBrow;
-    private double smile;
-    private boolean blink;
 
-    private double interest;
-    private double engagement;
-    private double stress;
-    private double relaxation;
-    private double excitement;
-    private double focus;
-
-    public Parameters(double time, 
-                        double neutral, 
-                        double pushSkill, 
-                        double overallSkill, 
-                        double raiseBrow, 
-                        double smile, 
-                        boolean blink,
-                        double interest,
-                        double engagement,
-                        double stress,
-                        double relaxation,
-                        double excitement,
-                        double focus) {
-        setTime(time);
-        setNeutral(neutral);
-        setPushSkill(pushSkill);
-        setOverallSkill(overallSkill);
-        setRaiseBrow(raiseBrow);
-        setSmile(smile);
-        setBlink(blink);
-        setInterest(interest);
-        setEngagement(engagement);
-        setStress(stress);
-        setRelaxation(relaxation);
-        setExcitement(excitement);
-        setFocus(focus);
+    public Parameters(Eye eye, LowerFace lowerFace, UpperFace upperFace,
+                        MentalCmd mentalCmd, Performance performance, double time) {
+        this.eye = eye;
+        this.lowerFace = lowerFace;
+        this.upperFace = upperFace;
+        this.mentalCmd = mentalCmd;
+        this.performance = performance;
+        this.time = time;
     }
 
     /**
@@ -59,171 +39,73 @@ public class Parameters {
     }
 
     /**
-     * @param neutral the neutral to set
+     * @param eye the eye to set
      */
-    public void setNeutral(double neutral) {
-        this.neutral = neutral;
+    public void setEye(Eye eye) {
+        this.eye = eye;
     }
 
     /**
-     * @return the neutral
+     * @return the eye
      */
-    public double getNeutral() {
-        return neutral;
+    public Eye getEye() {
+        return eye;
     }
 
     /**
-     * @param pushSkill the pushSkill to set
+     * @param lowerFace the lowerFace to set
      */
-    public void setPushSkill(double pushSkill) {
-        this.pushSkill = pushSkill;
-    }
-
-    /**
-     * @return the pushSkill
-     */
-    public double getPushSkill() {
-        return pushSkill;
-    }
-
-    /**
-     * @param overallSkill the overallSkill to set
-     */
-    public void setOverallSkill(double overallSkill) {
-        this.overallSkill = overallSkill;
-    }
-
-    /**
-     * @return the overallSkill
-     */
-    public double getOverallSkill() {
-        return overallSkill;
-    }
-
-    /**
-     * @param raiseBrow the raiseBrow to set
-     */
-    public void setRaiseBrow(double raiseBrow) {
-        this.raiseBrow = raiseBrow;
-    }
-
-    /**
-     * @return the raiseBrow
-     */
-    public double getRaiseBrow() {
-        return raiseBrow;
-    }
-
-    /**
-     * @param smile the smile to set
-     */
-    public void setSmile(double smile) {
-        this.smile = smile;
-    }
-
-    /**
-     * @return the smile
-     */
-    public double getSmile() {
-        return smile;
-    }
-
-    /**
-     * @param blink the blink to set
-     */
-    public void setBlink(boolean blink) {
-        this.blink = blink;
-    }
-
-    /**
-     * @return the blink
-     */
-    public boolean getBlink() {
-        return blink;
-    }
-
-    /**
-     * @param interest the interest to set
-     */
-    public void setInterest(double interest) {
-        this.interest = interest;
-    }
-
-    /**
-     * @return the interest
-     */
-    public double getInterest() {
-        return interest;
-    }
-
-    /**
-     * @param engagement the engagement to set
-     */
-    public void setEngagement(double engagement) {
-        this.engagement = engagement;
-    }
-
-    /**
-     * @return the engagement
-     */
-    public double getEngagement() {
-        return engagement;
-    }
-
-    /**
-     * @param stress the stress to set
-     */
-    public void setStress(double stress) {
-        this.stress = stress;
-    }
-
-    /**
-     * @return the stress
-     */
-    public double getStress() {
-        return stress;
-    }
-
-    /**
-     * @param relaxation the relaxation to set
-     */
-    public void setRelaxation(double relaxation) {
-        this.relaxation = relaxation;
-    }
-
-    /**
-     * @return the relaxation
-     */
-    public double getRelaxation() {
-        return relaxation;
-    }
-
-    /**
-     * @param excitement the excitement to set
-     */
-    public void setExcitement(double excitement) {
-        this.excitement = excitement;
-    }
-
-    /**
-     * @return the excitement
-     */
-    public double getExcitement() {
-        return excitement;
-    }
-
-    /**
-     * @param focus the focus to set
-     */
-    public void setFocus(double focus) {
-        this.focus = focus;
+    public void setLowerFace(LowerFace lowerFace) {
+        this.lowerFace = lowerFace;
     }
     
     /**
-     * @return the focus
+     * @return the lowerFace
      */
-    public double getFocus() {
-        return focus;
+    public LowerFace getLowerFace() {
+        return lowerFace;
+    }
+
+    /**
+     * @param mentalCmd the mentalCmd to set
+     */
+    public void setMentalCmd(MentalCmd mentalCmd) {
+        this.mentalCmd = mentalCmd;
+    }
+
+    /**
+     * @return the mentalCmd
+     */
+    public MentalCmd getMentalCmd() {
+        return mentalCmd;
+    }
+
+    /**
+     * @param performance the performance to set
+     */
+    public void setPerformance(Performance performance) {
+        this.performance = performance;
+    }
+
+    /**
+     * @return the performance
+     */
+    public Performance getPerformance() {
+        return performance;
+    }
+
+    /**
+     * @param upperFace the upperFace to set
+     */
+    public void setUpperFace(UpperFace upperFace) {
+        this.upperFace = upperFace;
+    }
+
+    /**
+     * @return the upperFace
+     */
+    public UpperFace getUpperFace() {
+        return upperFace;
     }
     
 }
