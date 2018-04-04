1bc6e26352224fe780ff1509c12e5a4cf66847ac
Lei Zhang <zleizju@gmail.com>
2018-04-04 10:40:58 -0700

    change facial panel

--- client/view/FacialPanel.java
+++ client/view/FacialPanel.java
@@ -3,6 +3,8 @@ package client.view;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.QuadCurve2D;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -19,30 +21,31 @@ public class FacialPanel extends JPanel{
 	private double x_factor, y_factor;
 	private int x_origin, y_origin;
 	private Parameters param;
+	private Graphics2D g2;
 	
 	private static final double DEFAULT_SCALE = 100.0;
 	private static final int DEFAULT_FACE_COORD = 20;
 	private static final int DEFAULT_FACE_SIZE = 60;
-	private static final int eye_radiusval = 5;
-	private static final int eye_left_xpos = 40;
-	private static final int eye_right_xpos = 60;
-	private static final int eye_ypos = 40;
-	private static final int pupil_size = 2;
-	private static final int eyebrow_left_left_x = 35;
-	private static final int eyebrow_left_right_x = 45;
-	private static final int eyebrow_right_left_x = 55;
-	private static final int eyebrow_right_right_x = 65;
-	private static final int eyebrow_y = 30;
-	private static final int mouth_ypos = 65;
-	private static final int nose_apex_x = 50;
-	private static final int nose_apex_y = 45;
-	private static final int nose_ypos = 54;
-	private static final int nose_width = 8;
+	
+	private static final int LEFT_EYE_POSX = 40;
+	private static final int RIGHT_EYE_POSX = 60;
+	private static final int EYE_POSY = 40;
+	private static final int EYE_WIDTH = 5;
+	private static final int EYE_HEIGHT = 3;
+	private static final int PUPIL_RADIUS = 2;
+	
+	private static final int EYEBROW_POSY = 33;
+	private static final int EYEBROW_HEIGHT = 3;
+	
+	private static final int MOUTH_POSX = 50;
+	private static final int MOUTH_POSY = 65;
+	private static final int MOUTH_WIDTH = 10;
+	private static final int MOUTH_HEIGHT = 6;
 	
 	public FacialPanel() {
-		Eye eye = new Eye(true, true, true, true, true);
-		LowerFace lowerFace = new LowerFace(1.0, 1.0, 1.0, 1.0, 1.0);
-		UpperFace upperFace = new UpperFace(1.0, 1.0);
+		Eye eye = new Eye(false, true, true, false, true);
+		LowerFace lowerFace = new LowerFace(1.0, 0.0, 1.0, 1.0, 1.0);
+		UpperFace upperFace = new UpperFace(0.0, 1.0);
 		MentalCmd mentalCmd = new MentalCmd(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
 		PerformanceMet performanceMet = new PerformanceMet(1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
 		double time = 1.0;
@@ -74,8 +77,12 @@ public class FacialPanel extends JPanel{
 		calc_scaleFactors(0,0,this.getHeight(),this.getWidth());
 		
 		drawFace(graph);
+		g2 = (Graphics2D) graph;
 		if (param!=null) {
 			drawEye(graph, param.getEye());
+			drawPupil(graph, param.getEye());
+			drawEyebrow(graph, param.getUpperFace());
+			drawMouth(graph, param.getLowerFace());
 			//TODO: draw other components for logic test
 		}
 	}
@@ -87,21 +94,73 @@ public class FacialPanel extends JPanel{
 	
 	public void drawEye(Graphics g, Eye eye) {
 		if(eye.getBlink() == true){
-			drawLine(g, eye_left_xpos + eye_radiusval, eye_ypos, eye_left_xpos - eye_radiusval, eye_ypos);
-			drawLine(g, eye_right_xpos - eye_radiusval, eye_ypos, eye_right_xpos + eye_radiusval, eye_ypos);
+			drawCurvedLine(g2, LEFT_EYE_POSX-EYE_WIDTH, EYE_POSY, LEFT_EYE_POSX, EYE_POSY+EYE_HEIGHT, LEFT_EYE_POSX+EYE_WIDTH, EYE_POSY);
+			drawCurvedLine(g2, RIGHT_EYE_POSX-EYE_WIDTH, EYE_POSY, RIGHT_EYE_POSX, EYE_POSY+EYE_HEIGHT, RIGHT_EYE_POSX+EYE_WIDTH, EYE_POSY);
 		}
-		else if(eye.getWinkLeft() == true){
+		else {
+			if(eye.getWinkLeft() == true){
+				drawCircle(g, LEFT_EYE_POSX-EYE_WIDTH, EYE_POSY-EYE_HEIGHT, 2*EYE_WIDTH, 2*EYE_HEIGHT);			
+			} else {
+				drawCurvedLine(g2, LEFT_EYE_POSX-EYE_WIDTH, EYE_POSY, LEFT_EYE_POSX, EYE_POSY+EYE_HEIGHT, LEFT_EYE_POSX+EYE_WIDTH, EYE_POSY);
+			}
 			
-			drawCircle(g, eye_left_xpos, eye_ypos, eye_radiusval, eye_radiusval);
-			drawLine(g, eye_right_xpos - eye_radiusval, eye_ypos, eye_right_xpos + eye_radiusval, eye_ypos);
+			if(eye.getWinkRight() == true){
+				drawCircle(g, RIGHT_EYE_POSX-EYE_WIDTH, EYE_POSY-EYE_HEIGHT, 2*EYE_WIDTH, 2*EYE_HEIGHT);
+			} else {
+				drawCurvedLine(g2, RIGHT_EYE_POSX-EYE_WIDTH, EYE_POSY, RIGHT_EYE_POSX, EYE_POSY+EYE_HEIGHT, RIGHT_EYE_POSX+EYE_WIDTH, EYE_POSY);
+			}
 		}
-		else if(eye.getWinkRight() == true){
-			drawLine(g, eye_left_xpos + eye_radiusval, eye_ypos, eye_left_xpos - eye_radiusval, eye_ypos);
-			drawCircle(g, eye_right_xpos, eye_ypos, eye_radiusval, eye_radiusval);
+	}
+	
+	public void drawPupil(Graphics g, Eye eye) {
+		if(eye.getBlink() != true){
+			if(eye.getWinkLeft() == true){
+				if (eye.getLookLeft()==true) {
+					drawCircle(g, LEFT_EYE_POSX-PUPIL_RADIUS-PUPIL_RADIUS, EYE_POSY-PUPIL_RADIUS, 2*PUPIL_RADIUS, 2*PUPIL_RADIUS);
+				} else if (eye.getLookRight()==true) {
+					drawCircle(g, LEFT_EYE_POSX-PUPIL_RADIUS+PUPIL_RADIUS, EYE_POSY-PUPIL_RADIUS, 2*PUPIL_RADIUS, 2*PUPIL_RADIUS);
+				} else {
+					drawCircle(g, LEFT_EYE_POSX-PUPIL_RADIUS, EYE_POSY-PUPIL_RADIUS, 2*PUPIL_RADIUS, 2*PUPIL_RADIUS);
+				}
+			}
+			if(eye.getWinkRight() == true){
+				if (eye.getLookLeft()==true) {
+					drawCircle(g, RIGHT_EYE_POSX-PUPIL_RADIUS-PUPIL_RADIUS, EYE_POSY-PUPIL_RADIUS, 2*PUPIL_RADIUS, 2*PUPIL_RADIUS);
+				} else if (eye.getLookRight()==true) {
+					drawCircle(g, RIGHT_EYE_POSX-PUPIL_RADIUS+PUPIL_RADIUS, EYE_POSY-PUPIL_RADIUS, 2*PUPIL_RADIUS, 2*PUPIL_RADIUS);
+				} else {
+					drawCircle(g, RIGHT_EYE_POSX-PUPIL_RADIUS, EYE_POSY-PUPIL_RADIUS, 2*PUPIL_RADIUS, 2*PUPIL_RADIUS);
+				}
+			}
 		}
-		else{
-			drawCircle(g, eye_left_xpos, eye_ypos, eye_radiusval, eye_radiusval);
-			drawCircle(g, eye_right_xpos, eye_ypos, eye_radiusval, eye_radiusval);
+	}
+	
+	public void drawEyebrow(Graphics g, UpperFace upperFace) {
+		if (upperFace.getFurrowBrow() > upperFace.getRaiseBrow()) {
+			int bias = Math.min(EYEBROW_HEIGHT, (int)(1+EYEBROW_HEIGHT*upperFace.getFurrowBrow()));
+			drawCurvedLine(g2, LEFT_EYE_POSX-EYE_WIDTH, EYEBROW_POSY, LEFT_EYE_POSX, EYEBROW_POSY+bias, LEFT_EYE_POSX+EYE_WIDTH, EYEBROW_POSY);
+			drawCurvedLine(g2, RIGHT_EYE_POSX-EYE_WIDTH, EYEBROW_POSY, RIGHT_EYE_POSX, EYEBROW_POSY+bias, RIGHT_EYE_POSX+EYE_WIDTH, EYEBROW_POSY);
+		} else {
+			int bias = Math.min(EYEBROW_HEIGHT, (int)(1+EYEBROW_HEIGHT*upperFace.getRaiseBrow()));
+			drawCurvedLine(g2, LEFT_EYE_POSX-EYE_WIDTH, EYEBROW_POSY, LEFT_EYE_POSX, EYEBROW_POSY-bias, LEFT_EYE_POSX+EYE_WIDTH, EYEBROW_POSY);
+			drawCurvedLine(g2, RIGHT_EYE_POSX-EYE_WIDTH, EYEBROW_POSY, RIGHT_EYE_POSX, EYEBROW_POSY-bias, RIGHT_EYE_POSX+EYE_WIDTH, EYEBROW_POSY);
+		}
+	}
+	
+	public void drawMouth(Graphics g, LowerFace lowerFace) {
+		if (lowerFace.getSmile()>0.5) {
+			int bias = Math.min(MOUTH_HEIGHT, (int)(1+MOUTH_HEIGHT*lowerFace.getSmile()));
+			drawCurvedLine(g2, MOUTH_POSX-MOUTH_WIDTH, MOUTH_POSY, MOUTH_POSX, MOUTH_POSY+bias, MOUTH_POSX+MOUTH_WIDTH, MOUTH_POSY);
+		} else {
+			if (lowerFace.getClench()>0.5) {
+				int bias = Math.min(MOUTH_HEIGHT, (int)(1+MOUTH_HEIGHT*lowerFace.getClench()));
+				drawCurvedLine(g2, MOUTH_POSX-MOUTH_WIDTH, MOUTH_POSY, MOUTH_POSX, MOUTH_POSY-bias, MOUTH_POSX+MOUTH_WIDTH, MOUTH_POSY);
+			} else {
+				if (lowerFace.getLaugh()>0.5) {
+					int bias = Math.min(MOUTH_HEIGHT*2, (int)(1+2*MOUTH_HEIGHT*lowerFace.getLaugh()));
+					drawCurvedLine(g2, MOUTH_POSX-MOUTH_WIDTH, MOUTH_POSY, MOUTH_POSX, MOUTH_POSY+bias, MOUTH_POSX+MOUTH_WIDTH, MOUTH_POSY);
+				}
+			}
 		}
 	}
 	
@@ -113,5 +172,13 @@ public class FacialPanel extends JPanel{
 	public void drawLine(Graphics g, int x1, int y1, int x2, int y2) {
 		g.drawLine(scale_x(x1) + x_origin, scale_y(y1) + x_origin,
 				scale_x(x2) + x_origin, scale_y(y2) + x_origin);
+	}
+	
+	public void drawCurvedLine(Graphics2D g2, int x1, int y1, int x2, int y2, int x3, int y3) {
+		
+		QuadCurve2D q = new QuadCurve2D.Float();
+		// draw QuadCurve2D.Float with set coordinates
+		q.setCurve(scale_x(x1), scale_y(y1), scale_x(x2), scale_y(y2), scale_x(x3), scale_y(y3));
+		g2.draw(q);
 	}
 }
--- service/MessageBox.java
+++
@@ -1,9 +0,0 @@
-package client.service;
-
-import javax.swing.*;
-
-public class MessageBox {
-    public void windowPop(String message){
-        JOptionPane.showMessageDialog(null, message,"alert",JOptionPane.ERROR_MESSAGE);
-    }
-}
