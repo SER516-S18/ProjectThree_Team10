12d1ddfda9fdc3e1266bd14cbe000cdb57675c33
divya-yadamreddi <dyadamre@asu.edu>
2018-04-05 17:08:56 -0700

    Update TimerClass.java

--- TimerClass.java
+++ TimerClass.java
@@ -1,23 +1,38 @@
-package com.timer;
+package server;
 
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/*
+ * Author: Shubham Vyas
+ * Description: Display time dynamically for server based on frequency selected.
+ */
+
 public class TimerClass {
 	private static int counter;
 	private static double z;
-	public static void main(String[] args) {
-		double x = 0.5;
-		int y = (int) (x * 1000);
-		z = x;
+	public static JLabel timeLabel = new JLabel();
+	public static String labelText;
+	
+	public static void timer(double frequency, JPanel time) {
+		
+		int y = (int) (frequency * 1000);
+		z = frequency;
 		Timer timer = new Timer();
 		try {
 			timer.scheduleAtFixedRate(new TimerTask(){ 
 				public void run() {
 					if(true) {
-						System.out.println(z+"\t"+counter++);
-						z += x;
+						//System.out.println(z+"\t"+counter++);
+						labelText = String.valueOf(z);
+						timeLabel.setText(labelText);
+						time.add(timeLabel);
+						z += frequency;
+						
 					}
 				} 
 			}, new Date(), y);
