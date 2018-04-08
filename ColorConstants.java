a8e473f3e699a8135ee786053091b704698ac7c9
harisiddarthvk <35482014+harisiddarthvk@users.noreply.github.com>
2018-04-07 21:34:08 -0700

    Added files containing Constants

---
+++ ColorConstants.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class ColorConstants {
+	
+	public static final Color WHITE = Color.WHITE;
+	public static final Color GRAY = Color.GRAY;
+	public static final Color BLACK = Color.BLACK;
+	public static final Color LIGHT_GRAY = Color.LIGHT_GRAY;
+
+}
