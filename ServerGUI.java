ad168cddcdfd2d78d95f88bacaaa799bde3964b2
harisiddarthvk <35482014+harisiddarthvk@users.noreply.github.com>
2018-04-07 21:32:18 -0700

    Refactor and add Console Functionality

--- ServerGUI.java
+++ ServerGUI.java
@@ -17,6 +17,7 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JEditorPane;
@@ -42,6 +43,7 @@ public class ServerGUI {
 	private JFrame composer;
 	private JPanel time;
 	private boolean flag = true;;
+	static JTextPane console = new JTextPane();
 	/**
 	 * Launch the application.
 	 */
@@ -74,53 +76,53 @@ public class ServerGUI {
 		
 		composer = new JFrame();
 		composer.setTitle("Emotive Composer");
-		composer.getContentPane().setBackground(Color.LIGHT_GRAY);
-		composer.getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		composer.getContentPane().setBackground(ColorConstants.LIGHT_GRAY);
+		composer.getContentPane().setFont(TextConstants.PLAIN);
 		composer.setBounds(100, 100, 631, 683);
 		composer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		composer.getContentPane().setLayout(null);
 		
 		JLabel labelTimeInterval = new JLabel("Time interval: ");
-		labelTimeInterval.setForeground(Color.WHITE);
-		labelTimeInterval.setBackground(Color.GRAY);
+		labelTimeInterval.setForeground(ColorConstants.WHITE);
+		labelTimeInterval.setBackground(ColorConstants.GRAY);
 		labelTimeInterval.setHorizontalAlignment(SwingConstants.CENTER);
-		labelTimeInterval.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		labelTimeInterval.setFont(TextConstants.PLAIN);
 		labelTimeInterval.setBounds(300, 32, 113, 28);
 		labelTimeInterval.setOpaque(true);
 		composer.getContentPane().add(labelTimeInterval);
 		
 		JCheckBox autoResetServer = new JCheckBox("Auto-reset");
-		autoResetServer.setForeground(Color.WHITE);
-		autoResetServer.setBackground(Color.GRAY);
-		autoResetServer.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		autoResetServer.setForeground(ColorConstants.WHITE);
+		autoResetServer.setBackground(ColorConstants.GRAY);
+		autoResetServer.setFont(TextConstants.PLAIN);
 		autoResetServer.setBounds(300, 73, 113, 25);
 		composer.getContentPane().add(autoResetServer);
 		
 		JButton start = new JButton("Start");
-		start.setForeground(Color.WHITE);
+		//start.setForeground(Color.WHITE);
 		start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		start.setBackground(Color.BLACK);
-		start.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		//start.setBackground(Color.BLACK);
+		start.setFont(TextConstants.PLAIN);
 		start.setBounds(425, 73, 97, 25);
-		start.setOpaque(true);
+		//start.setOpaque(true);
 		composer.getContentPane().add(start);
 		
 		JLabel labelTime = new JLabel("Time:");
-		labelTime.setForeground(Color.WHITE);
-		labelTime.setBackground(Color.GRAY);
-		labelTime.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		labelTime.setForeground(ColorConstants.WHITE);
+		labelTime.setBackground(ColorConstants.GRAY);
+		labelTime.setFont(TextConstants.PLAIN);
 		labelTime.setHorizontalAlignment(SwingConstants.CENTER);
 		labelTime.setBounds(38, 192, 80, 28);
 		labelTime.setOpaque(true);
 		composer.getContentPane().add(labelTime);
 		
 		JLabel labelSec = new JLabel("sec");
-		labelSec.setForeground(Color.WHITE);
+		labelSec.setForeground(ColorConstants.WHITE);
 		labelSec.setHorizontalAlignment(SwingConstants.LEFT);
-		labelSec.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		labelSec.setFont(TextConstants.PLAIN);
 		labelSec.setBounds(504, 38, 56, 16);
 		composer.getContentPane().add(labelSec);
 		
@@ -130,7 +132,7 @@ public class ServerGUI {
 		max = 99.99;
 		step = 0.50;
 		timeInterval.setModel(new SpinnerNumberModel(value, min, max, step));
-		timeInterval.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		timeInterval.setFont(TextConstants.PLAIN);
 		timeInterval.setBounds(425, 36, 73, 22);
 		composer.getContentPane().add(timeInterval);
 		
@@ -142,76 +144,76 @@ public class ServerGUI {
 		TimerClass.timer(frequency, time);
 		
 		JLabel labelSeconds = new JLabel("Seconds");
-		labelSeconds.setForeground(Color.WHITE);
-		labelSeconds.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		labelSeconds.setForeground(ColorConstants.WHITE);
+		labelSeconds.setFont(TextConstants.PLAIN);
 		labelSeconds.setHorizontalAlignment(SwingConstants.LEFT);
 		labelSeconds.setBounds(242, 194, 73, 25);
 		composer.getContentPane().add(labelSeconds);
 		
 		JLabel labelEmostate = new JLabel("EMOSTATE");
-		labelEmostate.setForeground(Color.WHITE);
-		labelEmostate.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		labelEmostate.setForeground(ColorConstants.WHITE);
+		labelEmostate.setFont(TextConstants.PLAIN);
 		labelEmostate.setHorizontalAlignment(SwingConstants.LEFT);
 		labelEmostate.setBackground(SystemColor.activeCaption);
 		labelEmostate.setBounds(22, 136, 113, 28);
 		composer.getContentPane().add(labelEmostate);
 		
 		JLabel labelEye = new JLabel("Eye:");
-		labelEye.setForeground(Color.WHITE);
-		labelEye.setBackground(Color.GRAY);
+		labelEye.setForeground(ColorConstants.WHITE);
+		labelEye.setBackground(ColorConstants.GRAY);
 		labelEye.setHorizontalAlignment(SwingConstants.LEFT);
-		labelEye.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		labelEye.setFont(TextConstants.PLAIN);
 		labelEye.setBounds(56, 245, 62, 26);
 		composer.getContentPane().add(labelEye);
 		
 		JComboBox<String> eyeOption = new JComboBox<String>();
-		eyeOption.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		eyeOption.setBackground(new Color(255, 255, 255));
+		eyeOption.setFont(TextConstants.PLAIN);
+		eyeOption.setBackground(ColorConstants.WHITE);
 		eyeOption.setToolTipText("");
 		eyeOption.setBounds(56, 272, 140, 28);
 		composer.getContentPane().add(eyeOption);
 		
 		JSpinner eyeValue = new JSpinner();
 		eyeValue.setModel(new SpinnerNumberModel(0, 0, 1, 1));
-		eyeValue.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		eyeValue.setFont(TextConstants.PLAIN);
 		eyeValue.setBounds(198, 272, 62, 27);
 		composer.getContentPane().add(eyeValue);
 		
 		JButton activateEye = new JButton("Activate");
-		activateEye.setForeground(Color.WHITE);
-		activateEye.setBackground(Color.BLACK);
-		activateEye.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		//activateEye.setForeground(Color.WHITE);
+		//activateEye.setBackground(Color.BLACK);
+		activateEye.setFont(TextConstants.PLAIN);
 		activateEye.setBounds(272, 272, 113, 28);
-		activateEye.setOpaque(true);
+		//activateEye.setOpaque(true);
 		composer.getContentPane().add(activateEye);
 		
 		JCheckBox autoResetEye = new JCheckBox("Auto-reset");
-		autoResetEye.setForeground(Color.WHITE);
-		autoResetEye.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		autoResetEye.setBackground(Color.GRAY);
+		autoResetEye.setForeground(ColorConstants.WHITE);
+		autoResetEye.setFont(TextConstants.PLAIN);
+		autoResetEye.setBackground(ColorConstants.GRAY);
 		autoResetEye.setBounds(397, 274, 113, 25);
 		composer.getContentPane().add(autoResetEye);
 		
 		JLabel labelUpperFace = new JLabel("Upperface:");
 		labelUpperFace.setHorizontalAlignment(SwingConstants.LEFT);
-		labelUpperFace.setForeground(Color.WHITE);
-		labelUpperFace.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		labelUpperFace.setBackground(Color.GRAY);
+		labelUpperFace.setForeground(ColorConstants.WHITE);
+		labelUpperFace.setFont(TextConstants.PLAIN);
+		labelUpperFace.setBackground(ColorConstants.GRAY);
 		labelUpperFace.setBounds(56, 328, 140, 26);
 		composer.getContentPane().add(labelUpperFace);
 		
 		JLabel labelLowerFace = new JLabel("Lowerface:");
 		labelLowerFace.setHorizontalAlignment(SwingConstants.LEFT);
-		labelLowerFace.setForeground(Color.WHITE);
-		labelLowerFace.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		labelLowerFace.setBackground(Color.GRAY);
+		labelLowerFace.setForeground(ColorConstants.WHITE);
+		labelLowerFace.setFont(TextConstants.PLAIN);
+		labelLowerFace.setBackground(ColorConstants.GRAY);
 		labelLowerFace.setBounds(300, 328, 140, 26);
 		composer.getContentPane().add(labelLowerFace);
 		
 		JComboBox<String> upperFaceOption = new JComboBox<String>();
 		upperFaceOption.setToolTipText("");
-		upperFaceOption.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		upperFaceOption.setBackground(Color.WHITE);
+		upperFaceOption.setFont(TextConstants.PLAIN);
+		upperFaceOption.setBackground(ColorConstants.WHITE);
 		upperFaceOption.setBounds(56, 356, 140, 28);
 		composer.getContentPane().add(upperFaceOption);
 		
@@ -221,61 +223,60 @@ public class ServerGUI {
 		max = 1.00;
 		step = 0.10;
 		upperFaceValue.setModel(new SpinnerNumberModel(value, min, max, step));
-		upperFaceValue.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		upperFaceValue.setFont(TextConstants.PLAIN);
 		upperFaceValue.setBounds(198, 356, 62, 27);
 		composer.getContentPane().add(upperFaceValue);
 		
 		JComboBox<String> lowerFaceOption = new JComboBox<String>();
 		lowerFaceOption.setToolTipText("");
-		lowerFaceOption.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		lowerFaceOption.setBackground(Color.WHITE);
+		lowerFaceOption.setFont(TextConstants.PLAIN);
+		lowerFaceOption.setBackground(ColorConstants.WHITE);
 		lowerFaceOption.setBounds(300, 356, 140, 28);
 		composer.getContentPane().add(lowerFaceOption);
 		
-	
-		
-		
 		JSpinner lowerFaceValue = new JSpinner();
 		lowerFaceValue.setModel(new SpinnerNumberModel(value, min, max, step));
-		lowerFaceValue.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		lowerFaceValue.setFont(TextConstants.PLAIN);
 		lowerFaceValue.setBounds(442, 357, 62, 25);
 		composer.getContentPane().add(lowerFaceValue);
 		
 		JLabel labelPerformance = new JLabel("Performance Metrics:");
 		labelPerformance.setHorizontalAlignment(SwingConstants.LEFT);
-		labelPerformance.setForeground(Color.WHITE);
-		labelPerformance.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		labelPerformance.setBackground(Color.GRAY);
+		labelPerformance.setForeground(ColorConstants.WHITE);
+		labelPerformance.setFont(TextConstants.PLAIN);
+		labelPerformance.setBackground(ColorConstants.GRAY);
 		labelPerformance.setBounds(56, 408, 165, 26);
 		composer.getContentPane().add(labelPerformance);
 		
 		JComboBox<String> performance = new JComboBox<String>();
 		performance.setToolTipText("");
-		performance.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		performance.setBackground(Color.WHITE);
+		performance.setFont(TextConstants.PLAIN);
+		performance.setBackground(ColorConstants.WHITE);
 		performance.setBounds(56, 436, 151, 28);
 		composer.getContentPane().add(performance);
 		
-	
-		
 		JLabel labelConsole = new JLabel("EMOENGINE LOG");
 		labelConsole.setHorizontalAlignment(SwingConstants.LEFT);
-		labelConsole.setForeground(Color.WHITE);
-		labelConsole.setFont(new Font("Times New Roman", Font.BOLD, 18));
-		labelConsole.setBackground(Color.GRAY);
+		labelConsole.setForeground(ColorConstants.WHITE);
+		labelConsole.setFont(TextConstants.PLAIN);
+		labelConsole.setBackground(ColorConstants.GRAY);
 		labelConsole.setBounds(22, 489, 165, 26);
 		composer.getContentPane().add(labelConsole);
 		
-		JEditorPane console = new JEditorPane();
-		console.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		
+		console.setFont(TextConstants.PLAIN);
 		console.setBounds(56, 522, 384, 89);
 		composer.getContentPane().add(console);
+		console.setEditable(false);
+		//ServerConsole.setMessage("Message");
+		//ServerConsole.setErrorMessage("Error");
 		
 		JPanel borderPanel2 = new JPanel();
-		borderPanel2.setBackground(Color.GRAY);
+		borderPanel2.setBackground(ColorConstants.GRAY);
 		borderPanel2.setBounds(12, 116, 589, 356);
 		borderPanel2.setBorder(BorderFactory.createLineBorder(Color.black));
 		composer.getContentPane().add(borderPanel2);
+		
 
 		Indicator indicatorPanel = new Indicator(0);
 		indicatorPanel.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -297,15 +298,15 @@ public class ServerGUI {
 		composer.getContentPane().add(borderPanel1);
 		
 		JButton clearLog = new JButton("Clear Log");
-		clearLog.setOpaque(true);
-		clearLog.setForeground(Color.WHITE);
-		clearLog.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		clearLog.setBackground(Color.BLACK);
+		//clearLog.setOpaque(true);
+		//clearLog.setForeground(Color.WHITE);
+		clearLog.setFont(TextConstants.PLAIN);
+		//clearLog.setBackground(Color.BLACK);
 		clearLog.setBounds(451, 580, 121, 31);
 		composer.getContentPane().add(clearLog);
 		
 		JPanel borderPanel3 = new JPanel();
-		borderPanel3.setBackground(Color.GRAY);
+		borderPanel3.setBackground(ColorConstants.GRAY);
 		borderPanel3.setBounds(12, 477, 589, 146);
 		borderPanel3.setBorder(BorderFactory.createLineBorder(Color.black));
 		composer.getContentPane().add(borderPanel3);
@@ -315,11 +316,9 @@ public class ServerGUI {
 		lowerFaceOption.addItem("Smirk Left");
 		lowerFaceOption.addItem("Smirk Right");
 		lowerFaceOption.addItem("Laugh");
-		
-		
+			
 		upperFaceOption.addItem("Raise Brow");
 		upperFaceOption.addItem("Furrow Brow");
-
 		
 		eyeOption.addItem("Blink");
 		eyeOption.addItem("wink left");
@@ -333,6 +332,15 @@ public class ServerGUI {
 		performance.addItem("4");
 		
 		hashvalues.initialize();
+		
+		clearLog.addActionListener( new ActionListener()
+		{
+		    @Override
+		    public void actionPerformed(ActionEvent e)
+		    {
+		        console.setText("");
+		    }
+		});
 		
         lowerFaceOption.addItemListener(new ItemListener() {
             @Override
@@ -398,10 +406,11 @@ public class ServerGUI {
     		   }
         	   timer.cancel();
     		   }
-    });
-      
-      
-    	   
-       }
+    	   });     	   
+	}
+	public static JTextPane getConsole(){
+		return console;
+	}
 }
+
 
