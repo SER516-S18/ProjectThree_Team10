a99bb38b71fdf27f9038527607b2d8368d9dbd74
Kanchan-Wakchaure <35479753+Kanchan-Wakchaure@users.noreply.github.com>
2018-04-10 13:50:59 -0700

    Add files via upload

---
+++ util/TextConstants.java
@@ -0,0 +1,9 @@
+package util;
+
+import java.awt.Font;
+
+public class TextConstants {
+
+	public static final Font PLAIN = new Font("Times New Roman", Font.PLAIN, 18);
+	public static final Font BOLD = new Font("Times New Roman", Font.BOLD, 18);
+}
---
+++ util/Variables.java
@@ -0,0 +1,46 @@
+package util;
+
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+
+public class Variables {
+
+	 
+	 public void getLowerFace(JComboBox<String> lowerFaceOption, JSpinner lowerFaceValue){
+		 String lowerFaceString = (String)lowerFaceOption.getSelectedItem();
+		 Double lowerFaceVal= hashvalues.lowerFace.get(lowerFaceString);
+		 lowerFaceValue.setValue(Double.valueOf(lowerFaceVal));
+	 }
+	 public void setLowerFace(JComboBox<String> lowerFaceOption, JSpinner lowerFaceValue){
+		 String lowerFaceString = (String)lowerFaceOption.getSelectedItem();
+		 Object lfv =  lowerFaceValue.getValue();
+		 hashvalues.lowerFace.put(lowerFaceString,  (Double) lfv);
+	 }
+	 
+	 public void getUpperFace(JComboBox<String> upperFaceOption, JSpinner upperFaceValue){
+		 String upperFaceString = (String)upperFaceOption.getSelectedItem();
+		 Double upperFaceVal= hashvalues.upperFace.get(upperFaceString);
+		 upperFaceValue.setValue(Double.valueOf(upperFaceVal));
+	 }
+	 public void setUpperFace(JComboBox<String> upperFaceOption, JSpinner upperFaceValue){
+		 String upperFaceString = (String)upperFaceOption.getSelectedItem();
+		 Object ufv =  upperFaceValue.getValue();
+		 hashvalues.upperFace.put(upperFaceString,  (Double) ufv);
+	 }
+	 
+	 public void getEye(JComboBox<String> eyeOption, JSpinner eyeValue){
+		 String eyeString = (String)eyeOption.getSelectedItem();
+		 Integer eyeVal= hashvalues.eyes.get(eyeString);
+		 eyeValue.setValue(Integer.valueOf(eyeVal));
+	 }
+	 public void setEye(JComboBox<String> eyeOption, JSpinner eyeValue){
+		 String eyeString = (String)eyeOption.getSelectedItem();
+		 Object efv =  eyeValue.getValue();
+		 hashvalues.eyes.put(eyeString,  (Integer) efv); 
+		 eyeValue.setValue(efv);
+		 
+	 }
+	 
+
+	 
+}
