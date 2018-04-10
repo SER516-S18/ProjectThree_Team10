4c1a65a0da09d330053cf1a1012a5a7fe1196117
eastalent <35270482+eastalent@users.noreply.github.com>
2018-04-10 16:32:41 -0700

    update gitig

--- src/server/service/TimerClass.java
+++ src/server/service/TimerClass.java
@@ -1,148 +1,74 @@
 package server.service;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import server.view.ServerGUI;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 /**
- * 
- * A abstract timer utility class, that supports pause and resume.
- * 
- * Can be used either as a normal timer or a countdown timer.
- * 
- * @author Sridhar Sundar Raman
- * 
+ * SER516 Project3_Team10
+ * Description: Class to run the timer on Server
+ * @author Shubham Vyas
+ * @author Hari Siddarth V Kesavan
+ * @version 1.0
  */
-public abstract class TimerClass {
-	public static final int DURATION_INFINITY = -1;
-	private volatile boolean isRunning = false;
-	private long interval;
-	private long elapsedTime;
-	private long duration;
-	private ScheduledExecutorService execService = Executors
-			.newSingleThreadScheduledExecutor();
-	private Future<?> future = null;
-	
-	
-	/**
-	 * Default constructor which sets the interval to 1000 ms (1s) and the
-	 * duration to {@link Timer#DURATION_INFINITY}
-	 */
-	public TimerClass() {
-		this(1000, -1);
+
+public class TimerClass{
+	private boolean isAutoReset = false;
+	private Timer timer;
+	private double timeCounter = (double) 0.0;
+	private static TimerClass timerObject = null;
+
+	protected TimerClass() {}
+
+	public static TimerClass getInstance() {
+		if (timerObject == null) {
+			timerObject = new TimerClass();
+		}
+		return timerObject;
 	}
 
 	/**
-	 * @param interval
-	 *            The time gap between each tick in millis.
-	 * @param duration
-	 *            The period in millis for which the timer should run. Set it to {@code Timer#DURATION_INFINITY} if the timer has to run indefinitely.
+	 * Timer Start
+	 *
+	 * @param delay - delay between two messages
 	 */
-	public TimerClass(long interval, long duration ) {
-		this.interval = interval;
-		this.duration = duration;
-		this.elapsedTime = 0;
-		
-	}
-
-	
-	public void abc(Long interval)
-	{
-		long ab = interval.longValue();
-		this.interval = ab;
-	}
-
-	/**
-	 * Starts the timer. If the timer was already running, this call is ignored.
-	 */
-	public void start(JLabel labelTimeDuration) {
-		if (isRunning)
-			return;
-
-		isRunning = true;
-		future = execService.scheduleWithFixedDelay(new Runnable() {
-			@Override
-			public void run() {
-				onTick();
-				elapsedTime += TimerClass.this.interval;
-				String et = Long.toString(elapsedTime/1000);
-				labelTimeDuration.setText(et);
-				if (duration > 0) {
-					if(elapsedTime >=duration){
-						onFinish();
-						future.cancel(false);
-					}
-				}
+	public void startTimer(double delay) {
+		timer = new Timer((int) Math.round(delay * 1000), new ActionListener() {
+			public void actionPerformed( ActionEvent event )  {
+				//TO-DO - send message to client
+				updateTimer(delay);
+				//ServerGUI.getTime().setText(getTimer() + "");
 			}
-		}, 0, this.interval, TimeUnit.MILLISECONDS);
+		});
+		timer.setRepeats(this.isAutoReset);
+		timer.start();
 	}
 
 	/**
-	 * Paused the timer. If the timer is not running, this call is ignored.
+	 * Timer Stop
 	 */
-	public void pause() {
-		if(!isRunning) return;
-		future.cancel(false);
-		isRunning = false;
+	public void stopTimer() {
+		timer.stop();
 	}
 
-	
 	/**
-	 * Resumes the timer if it was paused, else starts the timer.
+	 * Check for Auto-Reset
 	 */
-	public void resume(JLabel labelTimeDuration) {
-		this.start(labelTimeDuration);
+	public void setAutoReset(boolean autoRepeated) {
+		this.isAutoReset = autoRepeated;
 	}
 
-	
-	/**
-	 *	This method is called periodically with the interval set as the delay between subsequent calls. 
-	 */
-	protected abstract void onTick();
-	
-	
-	/**
-	 * This method is called once the timer has run for the specified duration. If the duration was set as infinity, then this method is never called.
-	 */
-	protected abstract void onFinish();
-
-	/**
-	 * Stops the timer. If the timer is not running, then this call does nothing.
-	 */
-	public void cancel() {
-		pause();
-		this.elapsedTime = 0;
+	void updateTimer(double delay) {
+		timeCounter += delay;
 	}
 
-	
-	/**
-	 * @return the elapsed time (in millis) since the start of the timer.
-	 */
-	public long getElapsedTime() {
-		return this.elapsedTime;
+	double getTimer() {
+		return timeCounter;
 	}
-	
-	/**
-	 * @return the time remaining (in millis) for the timer to stop. If the duration was set to {@code Timer#DURATION_INFINITY}, then -1 is returned.
-	 */
-	public long getRemainingTime(){
-		if(this.duration <0){
-			return TimerClass.DURATION_INFINITY;
-		}
-		else{
-			return duration-elapsedTime;
-		}
-	}
-	
-	/**
-	 * @return true if the timer is currently running, and false otherwise.
-	 */
-	public boolean isRunning() {
-		return isRunning;
+
+	public void setTimer(double time) {
+		timeCounter = time;
 	}
 }
