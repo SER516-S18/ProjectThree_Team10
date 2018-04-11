632aa4925d36a36b8d0ca908bea579458a5a2afb
byan17 <35480884+byan17@users.noreply.github.com>
2018-04-10 21:41:42 -0700

    add comment

--- src/client/service/ClientConfigurator.java
+++ src/client/service/ClientConfigurator.java
@@ -7,11 +7,20 @@ import java.util.List;
 import java.util.Map;
 
 public class ClientConfigurator extends Configurator {
+   
+    /**
+      * beforeRequest Method
+      * @param headers
+      */
     @Override
     public void beforeRequest(Map<String, List<String>> headers) {
         super.beforeRequest(headers);
     }
 
+    /**
+      * afterRequest Method
+      * @param handshakeResponse
+      */
     @Override
     public void afterResponse(HandshakeResponse handshakeResponse) {
         System.out.println(handshakeResponse);
